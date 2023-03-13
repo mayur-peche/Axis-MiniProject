e06aa3ce2b5b832122d154242584019388aa37f5
mayur-peche <mayurpeche.sitit@gmail.com>
2023-03-13 11:35:47 +0530

    "corrected some error in the custom query method declaration"

--- src/main/java/com/mal/repository/UserRepository.java
+++ src/main/java/com/mal/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.mal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mal.dto.UserDbDto;
 import com.mal.entity.UserDb;
 
 public interface UserRepository extends JpaRepository<UserDb, Integer>{
-	List<UserDbDto> getAnimeByStatus(String status);
+	
+	@Query("SELECT u FROM UserDb u WHERE u.status = :status")
+	List<UserDb> findByStatus(String status);
 }
