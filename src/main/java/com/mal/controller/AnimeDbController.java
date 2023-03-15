66f0752fdc62798eb4c1dc24853fc002511fe33a
mayur-peche <mayurpeche.sitit@gmail.com>
2023-03-15 23:54:56 +0530

    "CrossOrigin added for front end"

--- src/main/java/com/mal/controller/AnimeDbController.java
+++ src/main/java/com/mal/controller/AnimeDbController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.mal.dto.AnimeDbDto;
 import com.mal.service.AnimeDbService;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/anime")
 public class AnimeDbController {
 	
